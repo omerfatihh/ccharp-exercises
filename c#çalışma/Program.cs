4ff624f5e29c09b73b483f13d5c683d1528bb12b
Ömer Fatih Yeşilli <github-omerfatih0146@gmail.com>
2026-05-17 15:09:51 +0300

    Proje dosyası ekle.

---
+++ c#çalışma/Program.cs
@@ -0,0 +1,14 @@
+﻿ int Hesapla (int siparişAdedi)
+{
+    int pizzaPrice = siparişAdedi *150;
+    if (siparişAdedi >= 4)
+    {
+        pizzaPrice = (pizzaPrice* 90) / 100;
+    }
+        return pizzaPrice;
+}
+Console.Write(" Almak istediğiniz pizza adedini giriniz: ");
+int adet =(int.Parse(Console.ReadLine()));
+int ücret =Hesapla(adet);
+Console.WriteLine($"Pizzanızın fiyatı: {ücret} TL"); 
+Console.ReadKey();
